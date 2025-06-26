14f20c5b657d3ddb10a00e4092da0dda81cba342
saife24-ctrl <saif-eddine.biad.1@ens.etsmtl.ca>
2025-06-26 15:21:02 -0400

    move

--- Move.java
+++ Move.java
@@ -4,10 +4,14 @@ public class Move {
     int y;// 1-8
     char x;// A-H
     int pieceType;// 0 = empty, 1 = petitPionNoir, 2 = grandPionNoir, 3 = petitPionRouge, 4 = grandPionRouge
+    Board board = Client.board; // Assuming Client.board is accessible
 
-    public void movePiece(int x2, int y2, int pieceType) {
-       
+    public Move() {
+        this.y = 0;
+        this.x = 'A';
+        this.pieceType = 0; // Default to empty
     }
+
     
 
 
