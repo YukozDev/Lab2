7577148df667f6cca9f9a3f87af1e1dcefeeb09a
saife24-ctrl <saif-eddine.biad.1@ens.etsmtl.ca>
2025-06-26 14:46:14 -0400

    Logique pour deplacement de pièces

--- Move.java
+++ Move.java
@@ -5,6 +5,15 @@ public class Move {
     char x;
     int pieceType;
 
+    public Move(char x, char x2, int y, int y2, int pieceType) {
+        this.x = x;
+        this.y = y;
+        this.pieceType = pieceType;
+
+        System.out.println(pieceType + " " + x + y + "-" + x2 + y2);
+        
+    }
+
 
     
 }
