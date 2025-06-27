b6a94613e6822bda106f84bcb48b38172f7941c9
saife24-ctrl <saif-eddine.biad.1@ens.etsmtl.ca>
2025-06-27 14:11:07 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -18,4 +18,26 @@ public class Move {
     public void movePawn(Board board) {
 
     }
+
+    isValidMove(int x, int y, int nextX, int nextY){
+        // Check if the move is within bounds
+        if (nextX < 0 || nextX >= board.getSize() || nextY < 0 || nextY >= board.getSize()) {
+            return false;
+        }
+
+        // Check if the target cell is empty
+        if (board.getPown(nextX,nextY) != 0) {
+            return false;
+        }
+        if(board.getPown(x,y) == 3 && board.getPown(x-1,y-1) != 4 && board.getPown(x+1,y-1) != 4 && board.getPown(x,y-1) != 4){
+            return false;
+        }
+        if(board.getPown(x,y) == 1 && board.getPown(x-1,y+1) == 0 && board.getPown(x+1,y+1) == 0 && board.getPown(x,y+1) == 0){
+            return false;
+        }
+        
+
+        // Additional rules can be added here, such as checking for valid piece movement
+        return true;
+    }
 }
